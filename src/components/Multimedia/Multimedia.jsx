682dba35d8a477c95a85862165196ab0230b99ca
BtoMJ <danny_mj17@hotmail.com>
2022-04-27 13:28:56 -0500

    Componente about listo

--- src/components/Multimedia/Multimedia.jsx
+++ src/components/Multimedia/Multimedia.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
-import './Multimedia.css'
+import './Multimedia.css';
+import video from '../../assets/video.mp4'
 
 function Multimedia() {
   return (
     <div className="multimediaContainer">
-        Multimedia
+      <div className="multimediaInfo">
+        <div className="videoMultimedia">
+          <iframe className="video" 
+            src={video}>
+          </iframe>
+        </div>
+        <div className="videoDescripcion">
+          <h1>Como funcionan los suplementos</h1>
+          <p>
+            There are many variations of passages of Lorem Ipsum available, 
+            but the majority have suffered alteration in some form humour the 
+            and randomised words which don't look even slightly believable. 
+            If you are is going to use a passage of Lorem Ipsum.<br /><br />
+
+            There are many variations of passages of Lorem Ipsum available, 
+            but the majority have suffered alteration in some form humour the 
+            and randomised words which don't look even slightly believable. 
+            If you are is going to use a passage of Lorem Ipsum.
+          </p>
+          <a href="#">+ información</a>
+
+        </div>
+
+      </div>
     </div>
   )
 }
