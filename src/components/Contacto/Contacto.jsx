34424893ee0d0aa2d3b0c4bcd2344150ecbaa090
BtoMJ <danny_mj17@hotmail.com>
2022-05-10 01:58:24 -0500

    Responsive para celulares

--- src/components/Contacto/Contacto.jsx
+++ src/components/Contacto/Contacto.jsx
@@ -8,26 +8,27 @@ function Contacto() {
         <h3>tienes dudas</h3>
         <h1>Pónte en contacto con nosotros</h1>
         <form className="Form">
-          <div>
+          <div className="formContainer">
             <div className="Letrero">Nombre:</div>
             <input type="text" className="Caja" />
           </div>
 
-          <div>
+          <div className="formContainer">
             <div className="Letrero">Correo:</div>
             <input type="mail" className="Caja" />
           </div>
 
-          <div>
+          <div className="formContainer">
             <div className="Letrero">Teléfono:</div>
             <input type="tel" className="Caja" />
           </div>
 
-          <div>
+          <div className="formContainerArea">
             <div className="Letrero">Mensaje:</div>
             <textarea></textarea>
           </div>
-          <div>
+          
+          <div className="formContainer">
             <input type="submit" className="btnSend" value="Enviar" />
           </div>
         </form>
