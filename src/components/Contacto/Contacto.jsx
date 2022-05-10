e05495646ad8d2728f84139e2dfa3e5813c711f7
BtoMJ <danny_mj17@hotmail.com>
2022-05-09 23:30:59 -0500

    Formulario de Contacto listo

--- src/components/Contacto/Contacto.jsx
+++ src/components/Contacto/Contacto.jsx
@@ -4,7 +4,35 @@ import './Contacto.css';
 function Contacto() {
   return (
     <div className="contactoContainer">
-        Contacto
+      <div className="contactoForm">
+        <h3>tienes dudas</h3>
+        <h1>Pónte en contacto con nosotros</h1>
+        <form className="Form">
+          <div>
+            <div className="Letrero">Nombre:</div>
+            <input type="text" className="Caja" />
+          </div>
+
+          <div>
+            <div className="Letrero">Correo:</div>
+            <input type="mail" className="Caja" />
+          </div>
+
+          <div>
+            <div className="Letrero">Teléfono:</div>
+            <input type="tel" className="Caja" />
+          </div>
+
+          <div>
+            <div className="Letrero">Mensaje:</div>
+            <textarea></textarea>
+          </div>
+          <div>
+            <input type="submit" className="btnSend" value="Enviar" />
+          </div>
+        </form>
+      </div>
+        
     </div>
   )
 }
